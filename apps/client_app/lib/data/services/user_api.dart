31a7eb31fec692cf2ffe4d2148501ff4ee021263
Dreamwalker <aristojeff@gmail.com>
2025-01-04 16:41:19 +0900

    Update user_api.dart

--- apps/client_app/lib/data/services/user_api.dart
+++ apps/client_app/lib/data/services/user_api.dart
@@ -1,1 +1,15 @@
 
+import 'package:client_app/domain/models/user.dart';
+import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'user_api.g.dart';
+
+@RestApi(baseUrl: 'https://jsonplaceholder.typicode.com')
+abstract class UserApiClient {
+  factory UserApiClient(Dio dio, {String? baseUrl}) = _UserApiClient;
+
+  @GET('/users')
+  Future<List<User>> getUsers();
+}
