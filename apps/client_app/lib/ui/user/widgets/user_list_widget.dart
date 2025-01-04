cd0c25f84352f7d1ec53afc6b7c844dc5dfa617f
Dreamwalker <aristojeff@gmail.com>
2025-01-04 16:47:50 +0900

    Update user_list_widget.dart

--- apps/client_app/lib/ui/user/widgets/user_list_widget.dart
+++ apps/client_app/lib/ui/user/widgets/user_list_widget.dart
@@ -1,12 +1,13 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends ConsumerWidget {
   const MyWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     return const Placeholder();
   }
 }
