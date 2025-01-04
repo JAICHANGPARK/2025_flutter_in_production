532dd33eb49fb68fd58a0d71c1be1a9933fa6b40
Dreamwalker <aristojeff@gmail.com>
2025-01-04 17:38:15 +0900

    Create hacker_news_view_model.g.dart

---
+++ apps/client_app/lib/ui/hacker_news/view_model/hacker_news_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hacker_news_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchNewsHash() => r'14071e9a65d5b7cb86e168484e77c31e427986e7';
+
+/// See also [fetchNews].
+@ProviderFor(fetchNews)
+final fetchNewsProvider = AutoDisposeFutureProvider<List<Hn>>.internal(
+  fetchNews,
+  name: r'fetchNewsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$fetchNewsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FetchNewsRef = AutoDisposeFutureProviderRef<List<Hn>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
