301fde68864a35dd9bb8161d76493dfb998d7d7d
Dreamwalker <aristojeff@gmail.com>
2025-01-04 17:26:45 +0900

    Create hn_repository.g.dart

---
+++ apps/client_app/lib/data/repositories/hn_repository.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hn_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$hnRepositoryHash() => r'c7ba5ef736f2eb11452935ab572b480336068fae';
+
+/// See also [hnRepository].
+@ProviderFor(hnRepository)
+final hnRepositoryProvider = AutoDisposeProvider<HackerNewsApiClient>.internal(
+  hnRepository,
+  name: r'hnRepositoryProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$hnRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef HnRepositoryRef = AutoDisposeProviderRef<HackerNewsApiClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
